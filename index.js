cd36c927147e5e920a20cb1af01a501db9fc5579
Meriem <meriemgouader@gmail.com>
2024-09-07 19:22:51 +0200

    add MONGODB_URI

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 
-mongoose.connect("mongodb://localhost:27017/marvel");
+mongoose.connect(process.env.MONGODB_URI);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
